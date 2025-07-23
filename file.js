81cf58de31e321d99d66182c0fa3ef3073ee544b
Ali Abbas Abidi <smaliabbasa890@gmail.com>
2025-07-24 04:04:31 +0500

    profile added

---
+++ file.js
@@ -0,0 +1,9 @@
+const profile = {
+    username :"ALi_Abbas5",
+    posts : 23,
+    followers : 172,
+    following : 200,
+    bio : "Software Engineer, interested to explore technologies in software field",
+    gender : "Male"
+};
+console.log(profile)
